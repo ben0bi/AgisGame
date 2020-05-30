e2fe9678983886f949a5be01c3829caf208a5609
Nicht Krishna <krishnaexists@gmail.com>
2020-05-30 08:37:21 +0200

    Ein Agi bewegt sich und eben doch noch nicht...

--- js/game.js
+++ js/game.js
@@ -23,7 +23,7 @@ by Benedict Jäggi 2020
 
 // ***************************** OLD
 
-var aPlayer = function()
+var aCharacter = function()
 {
 	var me = this; // prevent sub functions from getting this-ed.
 	// lebenspunkte
@@ -32,20 +32,67 @@ var aPlayer = function()
 	this.addLP = function(value) {LP+=value;return LP;}
 	this.setLP = function(value) {LP=value;return LP;}
 	
+	// x and y position of the player.
+	var m_x = 10;
+	var m_y = 20;
+	// sprite name
 	var m_sprite = "agi";
+	// additional classes
 	var m_classes = "sprite";
-	var m_direction = "right";
+	// direction class
+	var m_direction = 5;
+	var m_dirname = -1; // set by m_direction
+	// the actual frame of this direction.
 	var m_dirFrame = 1;
+	var m_frameChange = 0.0; // time until next frame change.
+	var m_maxFrameChange = 0.15 // wait so long for next frame change.
+	var m_STATE = 1 // 0 = standing, 1 = moving
 	// update function
-	this.UPDATE = function()
+	this.UPDATE = function(deltatime)
 	{
-		m_myclass = m_sprite+" "+m_classes+" "+m_direction+"_"+m_dirFrame;
+		m_frameChange+=deltatime;
+		switch(m_STATE)
+		{
+			case 1: //moving
+				if(m_frameChange >= m_maxFrameChange)
+				{
+					m_dirFrame+=1
+					if(m_dirFrame>3)
+						m_dirFrame=1;
+				}
+				break;
+			case 0: // standing
+			default:
+				m_dirFrame = 3;
+				break;
+				
+		}
+		// reset framechange
+		if(m_frameChange >= m_maxFrameChange)
+			m_frameChange=0;
+
+		// translate direction
+		switch(m_direction)
+		{
+			case 4:
+			case 'u': m_dirname="up";break;
+			case 3:
+			case 'l': m_dirname = "left";break;
+			case 2:
+			case 'd': m_dirname = "down";break;
+			case 1:
+			case 'r':
+			default:
+				m_direction=1; // set default direction if there is a mess 
+				m_dirname="right";break;
+		}
+		m_myclass = m_sprite+" "+m_classes+" "+m_dirname+"_"+m_dirFrame;
 	}
 	
 	this.RENDER=function(screen)
 	{
 		log("Rendering to "+screen);
-		var elem='<div class="'+m_myclass+'" style="top: 10px; left: 10px;"></div>';
+		var elem='<div class="'+m_myclass+'" style="top: '+m_y+'px; left: '+m_x+'px;"></div>';
 		$(screen).append(elem);
 	}
 }
@@ -59,7 +106,7 @@ var aGame = function()
 	{
 		for(var i = 0;i<m_maxplayers;i++)
 		{
-			plr = new aPlayer();
+			plr = new aCharacter();
 			m_players.push(plr);
 		}
 		m_screen = myscreen;
@@ -72,7 +119,7 @@ var aGame = function()
 		for(var i=0;i<m_maxplayers;i++)
 		{
 			p=m_players[i];
-			p.UPDATE();
+			p.UPDATE(deltatime);
 			p.RENDER(m_screen);
 		}
 		
