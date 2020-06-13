0fbc83877ae14df5c3771d1fe157b2958163c635
Nicht Krishna <krishnaexists@gmail.com>
2020-06-13 17:31:51 +0200

    Map prepare for GML part one.

--- js/gamemap.js
+++ js/gamemap.js
@@ -22,11 +22,79 @@ Map & Map Editor
 
 */
 
+// the global parser holds some global values.
+var GMLParser_MAPGLOBAL = function()
+{
+	var me = this;
+	this.actualMapIntern = "";
+	this.startX=0;
+	this.startY=0;
+	this.startMapIntern = "";
+	this.parseGML = function(json, rootPath)
+	{
+		// get the start map. (StartMap)
+		// also set the actual room to the start room.
+		if(__defined(json['STARTMAP']))
+			me.actualMapIntern = me.startMapIntern = json['STARTMAP'];
+		// and the start position of the main character.
+		if(__defined(json['STARTPOSX']))
+			me.startX = json['STARTPOSX'];
+		if(__defined(json['STARTPOSY']))
+			me.startY = json['STARTPOSY'];
+		
+		// all images will be css-ed right now so no scale here
+		/* // get the global scale factor.
+		if(__defined(json['SCALEFACTOR']))
+			me.scaleFactor = parseFloat(json['SCALEFACTOR']);
+		if(__defined(json['SCALE']))
+			me.scaleFactor = parseFloat(json['SCALE']);
+		*/
+	}
+	this.clear = function()
+	{
+		me.actualMapIntern = "";
+		me.startMapIntern = "";
+		me.startX = 0;
+		me.startY = 0;
+	}
+}
+
+// a map tile in the tile view and on the map
+// those are different but in the same class here.
+// first gets CLASSES from gml.
+// second gets CLASSES from tile list, set with gml.
+var aMapTile = function()
+{
+	var me = this;
+	this.intern="";
+	this.classes =""
+	
+	// position is set wenn set on a map.
+	this.posX=0;
+	this.posY=0;
+	this.parseGML = function(json, rootPath)
+	{
+		// get the start map. (StartMap)
+		// also set the actual room to the start room.
+		if(__defined(json['INTERN']))
+			me.intern = json['INTERN'];
+		// and the start position of the main character.
+		if(__defined(json['CLASSES']))
+			me.classes = json['CLASSES'];
+	}
+}
+
+// a map holds map tiles with their intern names.
 var aGameMap = function()
 {
 	var me = this;
 	var sizex = 20;
 	var sizey = 20;
+	
+	this.parseGML = function(json, rootPath)
+	{
+		
+	}
 	
 	this.UPDATE=function(deltatime)
 	{
@@ -34,6 +102,7 @@ var aGameMap = function()
 	
 	this.RENDER=function()
 	{
+	
 	}
 }
 
